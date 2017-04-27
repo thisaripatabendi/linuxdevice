1c3ad2dfd7e06a4b3e654ed001b120e6ddbf2078
thisaripatabendi <thisaripatabendi@gmail.com>
2017-04-27 09:23:43 +0530

    Initial commit

---
+++ feature/feature/src/main/resources/dbscripts/mysql.sql
@@ -0,0 +1,10 @@
+
+CREATE  TABLE IF NOT EXISTS `linuxdevice_DEVICE` (
+  `linuxdevice_DEVICE_ID` VARCHAR(45) NOT NULL ,
+  `DEVICE_NAME` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`linuxdevice_DEVICE_ID`) )
+ENGINE = InnoDB;
+
+
+
+
